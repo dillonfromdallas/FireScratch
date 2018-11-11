5fa5127efb0b49f83249b7e9362ef4e1865277b5
dillonreyna <thedillonreyna@gmail.com>
2018-11-11 10:29:03 -0600

    Fixed onChange & onSubmit calls

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 class Signin extends Component {
   constructor() {
     super();
+
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
-
   onChange = e => {
     console.log(e);
   };
@@ -17,15 +17,15 @@ class Signin extends Component {
   render() {
     return (
       <div className="container">
-        <form onSubmit={this.onSubmit()} className="white">
+        <form onSubmit={this.onSubmit} className="white">
           <h5 className="grey-text text-darken-3">Sign In</h5>
           <div className="input-field">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={this.onChange()} />
+            <input type="email" id="email" onChange={this.onChange} />
           </div>
           <div className="input-field">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.onChange()} />
+            <input type="password" id="password" onChange={this.onChange} />
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Log In</button>

77571d8b4152db3e422f92cdb7e5a96b1ee8f27c
dillonreyna <thedillonreyna@gmail.com>
2018-11-11 09:31:05 -0600

    Base dashboard w mock data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Dashboard from "./components/dashboard/Dashboard";
 import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
@@ -9,6 +10,9 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path="/" component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ src/components/ideas/IdeaList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const IdeaList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Dev</p>
+          <p className="grey-text">Nov. 11th, 9:30am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Dev</p>
+          <p className="grey-text">Nov. 11th, 9:30am</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Dev</p>
+          <p className="grey-text">Nov. 11th, 9:30am</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IdeaList;

bc7ea7ac47cf92e08a9ee2c99eb16c0ece60e513
dillonreyna <thedillonreyna@gmail.com>
2018-11-14 10:59:42 -0600

    Linking firebase/firestore to redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,18 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
+import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
+import { getFirestore, reduxFirestore } from "redux-firestore";
 import thunk from "redux-thunk";
 
+import firebaseConfig from "../config/firebase";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reactReduxFirebase(firebaseConfig),
+    reduxFirestore(firebaseConfig)
+  )
+);
 
 export default store;

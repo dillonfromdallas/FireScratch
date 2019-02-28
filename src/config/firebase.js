5717023265d839b6434a4944d52989643dbe1315
dillonreyna <thedillonreyna@gmail.com>
2019-02-27 20:01:56 -0600

    Adding config for heroku

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAlM3TN1FqaIRp0wQeyTz0bEjSSD2Li7cE",
+  authDomain: "firescratch-b43e5.firebaseapp.com",
+  databaseURL: "https://firescratch-b43e5.firebaseio.com",
+  projectId: "firescratch-b43e5",
+  storageBucket: "firescratch-b43e5.appspot.com",
+  messagingSenderId: "42950246716"
+};
+
+export default firebaseConfig;
